04d8b9d5cba1c8cf77538b0266104e4685a0d25e
brianserem <brianseremiat@gmail.com>
2023-08-13 13:27:52 +0300

    added more components

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -44,11 +44,16 @@ const App = () =>
     setTasks(tasks.filter((task) => task.id !== id))
     console.log(id, 'has been deleted')
   }
+  // toggle task reminder
+
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? {...task, reminder : !task.reminder} : task))
+  }
 
   return (
     <div className="container">
       <Header/>
-      <Tasks tasks={ tasks } onDelete = {deleteTask} />
+      {tasks.length > 0 ? (<Tasks onToggle={toggleReminder} tasks={ tasks } onDelete = {deleteTask} />) : ('No more tasks!')}
     </div>
   );
 }
